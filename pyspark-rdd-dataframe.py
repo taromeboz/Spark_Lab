acdab7d822ec88facdf1564aa36770007b2fedaa
taromeboz <udom.ranok@gmail.com>
2021-08-28 22:39:22 +0700

    Create pyspark-rdd-dataframe.py

---
+++ pyspark-rdd-dataframe.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sat Jan 11 19:38:27 2020
+@author: sparkbyexamples.com
+'''
+
+dept = [("Finance",10), 
+        ("Marketing",20), 
+        ("Sales",30), 
+        ("IT",40) 
+      ]
+rdd = sc.parallelize(dept)
+
+df = rdd.toDF()
+df.printSchema()
+df.show(truncate=False)
+
+deptColumns = ["dept_name","dept_id"]
+df2 = rdd.toDF(deptColumns)
+df2.printSchema()
+df2.show(truncate=False)
+
+deptDF = spark.createDataFrame(data=dept, schema = deptColumns)
+deptDF.printSchema()
+deptDF.show(truncate=False)
+
+
+from pyspark.sql.types import StructType,StructField, StringType
+deptSchema = StructType([       
+    StructField('dept_name', StringType(), True),
+    StructField('dept_id', StringType(), True)
+])
+
+deptDF1 = spark.createDataFrame(data=dept, schema = deptSchema)
+deptDF1.printSchema()
+deptDF1.show(truncate=False)
