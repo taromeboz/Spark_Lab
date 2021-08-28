5d0b1c223fd0d2b6128a9c5c729ae812da82fb9e
taromeboz <udom.ranok@gmail.com>
2021-08-28 22:46:55 +0700

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -34,9 +34,4 @@ rdd6 = rdd5.filter(lambda x : 'a' in x[1])
 for element in rdd6.collect():
     print(element)
 
-from pyspark.sql.functions import col,expr
-data=[("2019-01-23",1),("2019-06-24",2),("2019-09-20",3)]
-spark.createDataFrame(data).toDF("date","increment") \
-    .select(col("date"),col("increment"), \
-      expr("add_months(to_date(date,'yyyy-MM-dd'),cast(increment as int))").alias("inc_date")) \
-    .show()
+
